7b4b1ea671d88718e3ec01373116c52211b2d9ab
quietsun <choi-hy@jptomato.com>
2022-10-23 19:58:13 +0900

    로직: 영화 평점별 폰트 컬러변경

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { useState, useEffect } from 'react';
 import MovieList from './components/MovieList';
+import './App.css';
 
 export default function App() {
   // JS
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    fetch('https://yts.mx/api/v2/list_movies.json?sort_by=rating')
+    fetch('https://yts.mx/api/v2/list_movies.json')
       .then(res => res.json())
       .then(json => {
         setMovies(json.data.movies);
